c91d5c0baaa17146d69d514aad240ac7040fc7ae
nano-rayhan <rr5345933@gmail.com>
2024-12-26 23:51:43 +0600

    Initial commit

---
+++ student1.java
@@ -0,0 +1,9 @@
+package java.student;
+
+public class student1 {
+    public int x;
+    public String name;  
+    public static void main(String args[]) {
+        System.out.println("Nano");
+    }
+}
